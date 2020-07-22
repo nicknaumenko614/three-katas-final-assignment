0ce4ae37e455580fc402a6502170d7009fef0664
Nick Naumenko <carswithcharacter614@gmail.com>
2020-07-22 19:48:55 -0400

    Added canTriangleBeConstructed method and two unit tests for the method tests pass

--- src/main/java/triangles/TriangleSorter.java
+++ src/main/java/triangles/TriangleSorter.java
@@ -4,4 +4,8 @@ public class TriangleSorter {
     public String analyze(int sideA, int sideB, int sideC) {
         return "";
     }
+
+    protected boolean canTriangleBeConstructed(int sideA, int sideB, int sideC) {
+        return (sideA + sideB) > sideC && (sideA + sideC) > sideB && (sideB + sideC) > sideA;
+    }
 }
--- src/test/java/triangles/TrianglesTest.java
+++ src/test/java/triangles/TrianglesTest.java
@@ -6,6 +6,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class TrianglesTest {
+
+    //we will start by testing if a triangle can be constructed with given sides:
+
+    @Test
+    public void canTriangleBeConstructedReturnsTrueWhenItCanBe() {
+        TriangleSorter underTest = new TriangleSorter();
+        boolean actualResult = underTest.canTriangleBeConstructed(3, 4, 5);
+        assertThat(actualResult).isTrue();
+
+        actualResult = underTest.canTriangleBeConstructed(5, 5, 3);
+        assertThat(actualResult).isTrue();
+
+        actualResult = underTest.canTriangleBeConstructed(3, 3, 3);
+        assertThat(actualResult).isTrue();
+    }
+
+    @Test
+    public void canTriangleBeConstructedReturnsFalseWhenItCanNotBe() {
+        TriangleSorter underTest = new TriangleSorter();
+        boolean actualResult = underTest.canTriangleBeConstructed(2, 3, 5);
+        assertThat(actualResult).isFalse();
+
+        actualResult = underTest.canTriangleBeConstructed(4, 7, 13);
+        assertThat(actualResult).isFalse();
+    }
+
     @Test
     public void threeEqualSidesIsAnEquilateralTriangle() {
         TriangleSorter underTest = new TriangleSorter();
