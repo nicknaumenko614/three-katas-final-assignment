ddfdbd82c3e846edf516982a7e64908d42f4da7c
lhumphries1 <65079306+lhumphries1@users.noreply.github.com>
2020-07-23 11:03:07 -0400

    fizz buzz kata 7-23-2020

--- src/test/java/fizz_buzz/FizzBuzzTest.java
+++ src/test/java/fizz_buzz/FizzBuzzTest.java
@@ -13,11 +13,70 @@ import static org.assertj.core.api.Assertions.assertThat;
  * -When the integer passed is a multiple of 3, the method should say Fizz.
  * -When the integer passed is a multiple of 5, the method should say Buzz.
  */
+
 public class FizzBuzzTest {
+
+    @Test
+    public void createFizzBuzzClass() {
+        FizzBuzz underTest = new FizzBuzz();
+    }
+
     @Test
     public void whenGiven1FizzBuzzSays1() {
         FizzBuzz underTest = new FizzBuzz();
         String spokenWord = underTest.say(1);
         assertThat(spokenWord).isEqualTo("1");
     }
+
+    @Test
+    public void whenGiven2FizzBuzzSays2() {
+        FizzBuzz underTest = new FizzBuzz();
+        String spokenWord = underTest.say(2);
+        assertThat(spokenWord).isEqualTo("2");
+    }
+
+    @Test
+    public void whenGiven3FizzBuzzSays3() {
+        FizzBuzz underTest = new FizzBuzz();
+        String spokenWord = underTest.say(3;
+        assertThat(spokenWord).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void whenGiven4FizzBuzzSays4() {
+        FizzBuzz underTest = new FizzBuzz();
+        String spokenWord = underTest.say(4);
+        assertThat(spokenWord).isEqualTo("4");
+    }
+
+    @Test
+    public void whenGiven5FizzBuzzSays5() {
+        FizzBuzz underTest = new FizzBuzz();
+        String spokenWord = underTest.say(5);
+        assertThat(spokenWord).isEqualTo("Buzz");
+    }
+
+    @Test
+    public void whenGiven6FizzBuzzSays6() {
+        FizzBuzz underTest = new FizzBuzz();
+        String spokenWord = underTest.say(6);
+        assertThat(spokenWord).isEqualTo("Fizz");
+    }
+
+    @Test
+    public void whenGiven7FizzBuzzSays7() {
+        FizzBuzz underTest = new FizzBuzz();
+        String spokenWord = underTest.say(7);
+        assertThat(spokenWord).isEqualTo("7");
+    }
+
+    @Test
+    public void whenGiven2FizzBuzzSays2() {
+        FizzBuzz underTest = new FizzBuzz();
+        String spokenWord = underTest.say(2);
+        assertThat(spokenWord).isEqualTo("2");
+    }
+
 }
+
+
