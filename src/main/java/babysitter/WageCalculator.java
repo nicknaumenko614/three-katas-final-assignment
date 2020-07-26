65c607999054083a816c4fd7910716cdcede6d6c
Nick Naumenko <carswithcharacter614@gmail.com>
2020-07-25 22:46:00 -0400

    created convertTime method and two unit tests that pass

--- src/main/java/babysitter/WageCalculator.java
+++ src/main/java/babysitter/WageCalculator.java
@@ -6,7 +6,11 @@ public class WageCalculator {
     }
 
 
-    public int convertTime(int inputTime) {
-        return 0;
+    protected static int convertTime(int inputTime) {
+        if (inputTime <= 24 && inputTime >= 17) {
+            return inputTime - 17;
+        } else {
+            return inputTime + 7;
+        }
     }
 }
--- src/test/java/babysitter/BabySitterTest.java
+++ src/test/java/babysitter/BabySitterTest.java
@@ -2,38 +2,34 @@ package babysitter;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class BabySitterTest {
 
 
-@Test
-public void convertTimeShouldMake17HoursZeroHours(){
-    WageCalculator underTest = new WageCalculator();
-    assertEquals(0, underTest.convertTime(17));
+    @Test
+    public void canConvertTimeBeforeMidnight() {
+        WageCalculator underTest = new WageCalculator();
+        int actualResult = underTest.convertTime(20);
+        assertThat(actualResult).isEqualTo(3);
+    }
 
-}
+    @Test
+    public void canConvertTimeAfterMidnight() {
+        WageCalculator underTest = new WageCalculator();
+        int actualResult = underTest.convertTime(2);
+        assertThat(actualResult).isEqualTo(9);
 
+        actualResult = underTest.convertTime(24);
+        assertThat(actualResult).isEqualTo(7);
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    @Test
-//    public void oneHourPreBedTimeShouldPay12() {
-//        WageCalculator underTest = new WageCalculator();
-//        int wage = underTest.calculateShiftWage(17, 18, 20);
-//        assertThat(wage).isEqualTo(12);
-//    }
+    @Test
+    public void oneHourPreBedTimeShouldPay12() {
+        WageCalculator underTest = new WageCalculator();
+        int wage = underTest.calculateShiftWage(17, 18, 20);
+        assertThat(wage).isEqualTo(12);
+    }
 }
